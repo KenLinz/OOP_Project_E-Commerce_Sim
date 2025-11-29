94be4211376616b5f0e8a085174d6f39ae1563b5
KenLinz <keli1246@colorado.edu>
2025-11-29 14:26:41 -0700

    SQL Database

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,2 @@
-INSERT INTO users (username, password, firstname, lastname, email) VALUES ('admin', 'admin', 'admin', 'admin', 'admin@gmail.com');
+INSERT INTO users (username, password, firstname, lastname, email, state) VALUES ('admin', '123', 'admin', 'admin', 'admin@gmail.com', 'CO');
 
